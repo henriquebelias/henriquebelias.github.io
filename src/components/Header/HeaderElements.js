f8c33d06a7e014fc24dfe61269af150641125c35
Henrique Brito Elias <henriquebelias@gmail.com>
2021-05-09 10:00:05 -0300

    teste

---
+++ src/components/Header/HeaderElements.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header` align-items: center;
+  background-color: black;
+  color: white;
+  display: flex;
+  height: 10vh;
+`;
+
+export const Menu = styled.div` font-size: 2rem;
+  margin: 0 auto;
+  width: 80%;
+`;
